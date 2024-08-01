9b48c49435d45e7b903143ad33d1269964e169ea
EarthSalamander <jeizyy30@hotmail.fr>
2024-08-01 09:34:48 +0200

    Add hour interval .env constant and fixed hour execution

--- speedtest.js
+++ speedtest.js
@@ -9,6 +9,7 @@ const SPEEDTEST_API_KEY = process.env.SPEEDTEST_API_KEY;
 const SERVER_NAME = process.env.SERVER_NAME;
 const SERVER_IMAGE = process.env.SERVER_IMAGE;
 const SERVER_COLOR = parseInt(process.env.SERVER_COLOR);
+const INTERVAL_HOURS = parseInt(process.env.INTERVAL_HOURS);
 
 let speedtest = new FastSpeedtest({
     token: SPEEDTEST_API_KEY,
@@ -22,8 +23,7 @@ let speedtest = new FastSpeedtest({
 
 client.once('ready', () => {
     console.log(`Connecté en tant que ${client.user.tag}!`);
-    runSpeedTest();
-    setInterval(runSpeedTest, 3600000);
+    scheduleNextRun();
 });
 
 async function runSpeedTest() {
@@ -44,6 +44,20 @@ async function runSpeedTest() {
     } catch (error) {
         console.error('Error during speed test:', error);
     }
+}
+
+function scheduleNextRun() {
+    const now = new Date();
+    const minutesToNextHour = 60 - now.getMinutes();
+    const secondsToNextHour = minutesToNextHour * 60;
+    const millisecondsToNextHour = secondsToNextHour * 1000;
+
+    console.log(`Le script s'exécutera dans ${minutesToNextHour} minutes.`);
+
+    setTimeout(() => {
+        runSpeedTest();
+        setInterval(runSpeedTest, INTERVAL_HOURS * 3600000); // INTERVAL_HOURS in milliseconds
+    }, millisecondsToNextHour);
 }
 
 client.login(TOKEN);
